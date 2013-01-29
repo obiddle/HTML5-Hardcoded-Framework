1d9ee7b3b7ed91a58c81f268050c8f70cbcee5a2
owen <obiddle@gmail.com>
2013-01-28 20:41:19 -0500

    new branch first

---
+++ click.js
@@ -0,0 +1,18 @@
+var nav = {
+
+turnPage: function()
+    {
+        $('.pageOne').addClass('pageOneActive');
+        $('.pageTwo').addClass('pageTwoActive');
+
+        
+    },
+
+    goBack: function()
+    {
+        $('.pageOne').removeClass('pageOneActive');
+        $('.pageTwo').removeClass('pageTwoActive');
+
+        
+    },
+}
